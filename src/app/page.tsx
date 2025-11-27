d2245e00c4a7b8699a2b7423eba9da68ab0f6d24
AJ <abduljaberaj00786@gmail.com>
2025-11-27 19:12:46 +0530

    (feat): update render of components
    - For Tauri based Desktop render ChatPage without Home related components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,24 @@ import Hero from "./_components/section/Hero";
 import Features from "./_components/section/Feature";
 import Faq from "./_components/section/FAQ";
 import Footer from "@/components/Footer";
+import ChatPage from "./chat/page";
+import { isTauri } from "@tauri-apps/api/core";
+
 export default function HomePage() {
   return (
     <div>
-      <Hero />
-      <Features />
-      <Faq />
-      <Footer />
+      {isTauri() ? (
+        <>
+          <ChatPage />
+        </>
+      ) : (
+        <>
+          <Hero />
+          <Features />
+          <Faq />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
