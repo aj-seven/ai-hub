2c4e92e937679b6101625e0f844ba7710b284fbb
aj-seven <abduljaberaj00786@gmail.com>
2026-02-18 10:58:35 +0530

    (feat): support url link open

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -159,6 +159,7 @@ fn greet() -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, call_api, chat_api, stream_api])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
